423ce7e06434fb121bf31e7812b46346e9f040db
DIMITRI VILERA SARAIVA DE AZEVEDO <dimitri.vsazevedo@senacsp.edu.br>
2022-05-05 16:25:22 -0300

    projeto tenis 05

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var tenis1=dunk
+var tenis2=air_force
+var tenis3=jordan
+var tenis4=air_max
+var tenis5=young
+var tenis6=superstar
+var tenis7=nmd
+var tenis8=celox
+var tenis9=suede
+var tenis10=rsz
+var tenis11=future
+var tenis12=rider
+var tenis13=old
+var tenis14=ultra
+var tenis15=authe
+var tenis16=sk8
